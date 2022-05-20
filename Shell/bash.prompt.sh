f484649df7cf9972c55357007deeeac8c37f5d96
Christian Engel <engel-ch@outlook.com>
2022-05-20 15:45:35 +0200

    watson check

--- Shell/bash.prompt.sh
+++ Shell/bash.prompt.sh
@@ -11,6 +11,8 @@ function bashPrompt() {
       PS1='[$?] \033[0;31m\t | \u@\h | $(pwd) \033[0m##########################\n'
    else
       debug8 bash non-root shell
+      unalias watson 2>/dev/null    # echo watson is $(which watson)
+      [ $(which watson | wc -l) -eq 0 ] && debug12 watson not found && alias watson='echo -- > /dev/null'
       PS1='[$?] \033[34m\t\033[0m|\033[32m\u@\h\033[0m|\033[34m$(watson status -p)$(watson status -t)\033[0m|\033[0;31m$(gitContents)\033[0m|$AWS_PROFILE|\033[0;33m\w\e[0m\n'
    fi
 }
