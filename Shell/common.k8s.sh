8b27deed7e5c188e34e63ad54f2b7ba0296f7488
Christian Engel <engel-ch@outlook.com>
2022-07-26 00:59:09 +0200

    k8s commands changed to use the environment variable KUBECTL for be reset for microk8s,...

--- Shell/common.k8s.sh
+++ Shell/common.k8s.sh
@@ -1,38 +1,39 @@
 # ---- k8s -----------------------------------------------------------------------
 
 function k8Setup() {
+   export KUBECTL=kubectl
    alias dfimage="docker run -v /var/run/docker.sock:/var/run/docker.sock --rm chenzj/dfimage" # find base image for a container OR docker image history
    alias dih='docker image history'
-   alias k=kubectl
-   alias k8=kubectl
-   alias k8c='kubectl config '
-   alias k8cg='kubectl config get-contexts'
-   alias k8cs='kubectl config set-context'
-   alias k8cu='kubectl config use-context'
-   alias k8cv='kubectl config view'
-   alias k8gn='kubectl get nodes -o wide'
-   alias k8gp='kubectl get pods -o wide'
-   alias k8gs='kubectl get services -o wide'
-   alias k8ns='kubectl get ns'
-   alias k8ga='kubectl get all -A -o wide'
-   alias kevents='kubectl get events --sort-by=.metadata.creationTimestamp'
+   alias k=$KUBECTL
+   alias k8=$KUBECTL
+   alias k8c='$KUBECTL config '
+   alias k8cg='$KUBECTL config get-contexts'
+   alias k8cs='$KUBECTL config set-context'
+   alias k8cu='$KUBECTL config use-context'
+   alias k8cv='$KUBECTL config view'
+   alias k8gn='$KUBECTL get nodes -o wide'
+   alias k8gp='$KUBECTL get pods -o wide'
+   alias k8gs='$KUBECTL get services -o wide'
+   alias k8ns='$KUBECTL get ns'
+   alias k8ga='$KUBECTL get all -A -o wide'
+   alias kevents='$KUBECTL get events --sort-by=.metadata.creationTimestamp'
    alias k8ev=kevents
 
    function k8describe() {
-      local _pod=$(kubectl get po | grep -i "$1" | cut -d ' ' -f 1)
+      local _pod=$($KUBECTL get po | grep -i "$1" | cut -d ' ' -f 1)
       shift
-      kubectl describe $_pod $*
+      $KUBECTL describe $_pod $*
    }
 
    function k8exec() {
-      local _pod=$(kubectl get po | grep -i "$1" | cut -d ' ' -f 1)
+      local _pod=$($KUBECTL get po | grep -i "$1" | cut -d ' ' -f 1)
       shift
-      kubectl exec -it $_pod -- ${k8execCmd:-bash} $*
+      $KUBECTL exec -it $_pod -- ${k8execCmd:-bash} $*
    }
 
    function k8logs() {
-      local _pod=$(kubectl get po | grep -i "$1" | cut -d ' ' -f 1)
-      kubectl logs "$2" $_pod # $2 for -f
+      local _pod=$($KUBECTL get po | grep -i "$1" | cut -d ' ' -f 1)
+      $KUBECTL logs "$2" $_pod # $2 for -f
    }
 
    # help file to show all k8s commands
