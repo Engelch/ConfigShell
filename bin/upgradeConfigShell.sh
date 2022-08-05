2efe10a2ec35391b1a30cd97e741bcb1d1439fb7
Christian ENGEL <engel-ch@outlook.com>
2022-08-05 21:55:25 +0200

    upgradeConfigDir: replacing updateConfigDir, updates every git repo with supplied argument, 4 hours aging  now configurable by variable UPDATE_CONFIGSHELL_FREQUENCE. Moved to a generic git pull upgrade technology.

---
+++ bin/upgradeConfigShell.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+flagFile=
+
+function isOlderThanHours() {
+    # $1 hours, uint, >0
+    # $2 filename to check
+    [ $# -ne 2 ] && 1>&2 echo Wrong call to isOlderThanHours with args $* && return 10
+    [ "$1" -lt 1 ] && 1>&2 echo hours argument $1 not correct && return 11
+    [ ! -f "$2" ] && 1>&2 echo Filename $2 is not a plain file && return 12
+    local currentTime=$(date -u '+%s')
+    local fileMTime=$(date  -u -r $2 '+%s')
+    local hoursInSecs=$(( $1 * 3600 ))
+    local fileAndHours=$(( $fileMTime + $hoursInSecs ))
+    #echo current $currentTime, fileMTime $fileMTime, hoursInSecs $hoursInSecs, file+hours $fileAndHours
+    [ $fileAndHours -ge $currentTime ] && return 1
+    return 0
+}
+
+function conditionalUpdateGitRepo() {
+    local res
+    echo -n doing updateConfigShell $* ..... ' '
+    [ ! -d "$1" ] && 1>&2 echo supplied argument not a directory $1 && return 10
+    if [ -f $flagFile ] ; then
+        if [ $(isOlderThanHours ${UPDATE_CONFIGSHELL_FREQUENCE:-4} $flagFile) ] ; then
+            echo upgrading...
+            cd "$1"
+            git pull
+            res=$?
+            touch $flagFile
+            cd $OLDPWD
+        else 
+            echo NOT upgrading
+            res=0
+        fi
+    else
+        echo upgrading
+        touch $flagFile 
+        cd "$1" 
+        git pull
+        res=$?
+        cd $OLDPWD
+    fi
+    return $res
+}
+
+function main() {
+    # update configShell
+    local res
+    if [ ! -z "$1" ] ; then
+        res=0
+        for file in $* ; do
+            echo in loop res $res
+            flagFile=$HOME/.upgradeConfigShell.$(basename $file)
+            [ ! -d "$file" ] && 1>&2 echo Supplied argument $file not a directory && (( res += 60 )) && continue
+            conditionalUpdateGitRepo "$file" ; (( res += $? ))
+            echo res is $res
+        done
+        return $res
+    else 
+        dir=$(dirname $(readlink -f $HOME/.bashrc))
+        flagFile=$HOME/.upgradeConfigShell.$(basename $dir)
+        conditionalUpdateGitRepo $dir
+        return $?
+    fi
+    # should never happen
+    return 90
+}
+
+main $*
+
+# EOF
