1b7185adbc6c55491b662445f9a309789e3e5bb7
Christian ENGEL <engel-ch@outlook.com>
2022-12-14 11:08:10 +0100

    k8cp with support for namespaces and containers is born

---
+++ Skeleton/bash.mini.skeleton.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+# vim: set expandtab: ts=3: sw=3
+#
+# TITLE: $_app
+#
+# DESCRIPTION: <see usage function below>
+#
+# LICENSE: MIT todo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+#########################################################################################
+# VARIABLES, CONSTANTS
+
+readonly _app=$(basename $0)
+readonly _appDir=$(dirname $0)
+readonly _absoluteAppDir=$(cd $_appDir; /bin/pwd)
+readonly _appVersion="0.0.1" # use semantic versioning
+export DebugFlag=${DebugFlag:-FALSE}
+
+#########################################################################################
+
+# --- debug: Conditional debugging. All commands begin w/ debug.
+
+function debugSet()             { DebugFlag=TRUE; return 0; }
+function debugUnset()           { DebugFlag=; return 0; }
+function debugExecIfDebug()     { [ "$DebugFlag" = TRUE ] && $*; return 0; }
+function debug()                { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:'$* 1>&2 ; return 0; }
+function debug4()               { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:    ' $* 1>&2 ; return 0; }
+function debug8()               { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:        ' $* 1>&2 ; return 0; }
+function debug12()              { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:            ' $* 1>&2 ; return 0; }
+
+# --- Exits
+
+# function error()        { err 'ERROR:' $*; return 0; } # similar to err but with ERROR prefix and possibility to include
+# Write an error message to stderr. We cannot use err here as the spaces would be removed.
+function error()        { echo 'ERROR:'$* 1>&2;             return 0; }
+function error4()       { echo 'ERROR:    '$* 1>&2;         return 0; }
+function error8()       { echo 'ERROR:        '$* 1>&2;     return 0; }
+function error12()      { echo 'ERROR:            '$* 1>&2; return 0; }
+
+function errorExit()    { EXITCODE=$1 ; shift; error $* ; exit $EXITCODE; }
+function exitIfErr()    { a="$1"; b="$2"; shift; shift; [ "$a" -ne 0 ] && errorExit $b App returned $a $*; }
+
+function err()          { echo $* 1>&2; }                 # just write to stderr
+function err4()         { echo '   ' $* 1>&2; }           # just write to stderr
+function err8()         { echo '       ' $* 1>&2; }       # just write to stderr
+function err12()        { echo '           ' $* 1>&2; }   # just write to stderr
+
+# --- Existance checks
+function exitIfBinariesNotFound()       { for file in $@; do [ $(command -v "$file") ] || errorExit 253 binary not found: $file; done }
+function exitIfPlainFilesNotExisting()  { for file in $*; do [ ! -f $file ] && errorExit 254 'plain file not found:'$file 1>&2; done }
+function exitIfFilesNotExisting()       { for file in $*; do [ ! -e $file ] && errorExit 255 'file not found:'$file 1>&2; done }
+function exitIfDirsNotExisting()        { for dir in $*; do [ ! -d $dir ] && errorExit 252 "$APP:ERROR:directory not found:"$dir; done }
+
+# --- Temporary file/directory  creation
+# -- file creation -- TMP1=$(tempFile); TMP2=$(tempFile) ;;;; trap "rm -f $TMP1 $TMP2" EXIT
+# -- directory creation -- TMPDIR=$(tempDir) ;;;;;  trap "rm -fr $TMPDIR;" EXIT
+#
+function tempFile()                     { mktemp ${TMPDIR:-/tmp/}$_app.XXXXXXXX; }
+function tempDir()                      { mktemp -d "${TMPDIR:-/tmp/}$_app.YYYYYYYYY"; }
+# realpath as shell, argument either supplied as stdin or as $1
+
+# application-specific functions  ===============================================================================
+
+function usage()
+{
+    err DESCRIPTION
+    err
+    err $_app
+    err SYNOPSIS
+    err4 $_app '[-D] [-f] [dir...]'
+    err4 $_app '-h'
+    err
+    err VERSION
+    err4 $_appVersion
+    err
+    err OPTIONS
+    err4 '-D      ::= enable debug output'
+    err4 '-h      ::= show usage message and exit with exit code 1'
+    err4 todo .......
+}
+
+function parseCLI() {
+    local currentOption
+    while getopts "Dfh" options; do         # Loop: Get the next option;
+        case "${options}" in                    # TIMES=${OPTARG}
+            D)  err Debug enabled ; debugSet
+                ;;
+            f)  debug forcedMode; forcedMode=TRUE
+                ;;
+            h)  usage ; exit 1
+                ;;
+            *)
+                err Help with $_app -h
+                exit 2  # Exit abnormally.
+                ;;
+        esac
+    done
+}
+
+function main() {
+    exitIfBinariesNotFound pwd basename dirname mktemp
+    parseCLI $*
+    shift $(($OPTIND - 1))  # not working inside parseCLI
+    debug args are $*
+    debug forcedMode is ${forcedMode:-FALSE}
+    echo todo '(search and replace all todo accordingly)' here more....................
+}
+
+main $*
+
+# EOF
