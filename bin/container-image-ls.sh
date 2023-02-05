34a483e96eff33f38a7925ad9ff5b38324c65cab
Christian Engel <engel-ch@outlook.com>
2023-02-05 23:34:27 +0100

    container-image-ls with architecture output

--- bin/container-image-ls.sh
+++ bin/container-image-ls.sh
@@ -1,33 +1,44 @@
 #!/usr/bin/env bash
 
-function err() {
-  1>&2 echo $*
+
+function debugSet()             { DebugFlag=TRUE; return 0; }
+function debugUnset()           { DebugFlag=; return 0; }
+function debugExecIfDebug()     { [ "$DebugFlag" = TRUE ] && "$*"; return 0; }
+function debug()                { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:'"$*" 1>&2 ; return 0; }
+function debug4()               { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:    ' "$*" 1>&2 ; return 0; }
+function debug8()               { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:        ' "$*" 1>&2 ; return 0; }
+function debug12()              { [ "$DebugFlag" = TRUE ] && echo 'DEBUG:            ' "$*" 1>&2 ; return 0; }
+
+function error()        { echo 'ERROR:'"$*" 1>&2;             return 0; }
+function error4()       { echo 'ERROR:    '"$*" 1>&2;         return 0; }
+function error8()       { echo 'ERROR:        '"$*" 1>&2;     return 0; }
+function error12()      { echo 'ERROR:            '"$*" 1>&2; return 0; }
+
+function errorExit()    { EXITCODE="$1" ; shift; error "$*" ; exit "$EXITCODE"; }
+function exitIfErr()    { a="$1"; b="$2"; shift; shift; [ "$a" -ne 0 ] && errorExit "$b" App returned "$a" "$*"; }
+
+function err()          { echo "$*" 1>&2; }                 # just write to stderr
+function err4()         { echo '   ' "$*" 1>&2; }           # just write to stderr
+function err8()         { echo '       ' "$*" 1>&2; }       # just write to stderr
+function err12()        { echo '           ' "$*" 1>&2; }   # just write to stderr
+
+# --- Existance checks
+function exitIfBinariesNotFound()       { for file in "$@"; do [ $(command -v "$file") ] || errorExit 253 binary not found: "$file" ; done }
+
+###########################
+
+# setDockerCmd
+function setDockerCmd() {
+   dockerCmd=
+   # Listed in order of ASCENDING preference (podman > docker)
+   which docker &>/dev/null && dockerCmd=docker
+   which podman &>/dev/null && dockerCmd=podman
+   [ -z "$dockerCmd" ] && errorExit 10 no docker command found
+   debug docker command is set to "$dockerCmd"
 }
 
-function errorExit() {
-  val=$1
-  shift
-  err 'ERROR:'$*
-  exit $val
-}
+exitIfBinariesNotFound jq paste
+setDockerCmd
+$dockerCmd image ls -n -q | paste -s -d ' ' - | xargs $dockerCmd inspect | \
+  jq '.[]|{ Names: .NamesHistory,Architecture: .Architecture, Id: .Id, Created: .Created, Cmd: .Config.Cmd[] }'
 
-function usage() {
-    err $(basename $0) '[ <<image-name>>...] '
-    err
-    err list matching or all images.
-}
-	
-# Listed in order of ASCENDING preference (podman > docker)
-which docker &>/dev/null &&
-CONTAINER_TOOL=docker
-which podman &>/dev/null &&
-CONTAINER_TOOL=podman
-
-if [ "$*" = '' ] ; then # all images
-    $CONTAINER_TOOL image ls
-else
-    for _image in $* ; do
-        $CONTAINER_TOOL image ls | grep $_image
-    done
-fi
-# eof
