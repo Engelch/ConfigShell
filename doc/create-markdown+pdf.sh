5817db471f343b89ab0af3661af917a0ab088406
Christian Engel <engel-ch@outlook.com>
2023-05-21 15:27:55 +0200

    doc/create-markdown+pdf.sh:improved documentation+shellcheck

--- doc/create-markdown+pdf.sh
+++ doc/create-markdown+pdf.sh
@@ -1,14 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # shellcheck disable=SC2155
 
-# About: create README.pdf and README.md from README.orig.md
-# Requirements: ConfigShell, pandoc
+# About:
+#   1. create PDF-files and Markdown-files in the parent directory from *.orig.md files in the current directory
+#   2. create PDF-files and Markdown-files in the parent directory from *.marp.md files
+# Requirements: ConfigShell, pandoc, marp
 # Author: engelch
 # c230507
-# u230508 (no version# of its own)
+# u230521 (no version# of its own)
 
+# check for minimal required version of md2pdf - not required if distributed as part of ConfigShell
 minor=$(md2pdf -V 2>&1 | sed -E 's/^[[:digit:]]+\.//' | sed -E 's/\..*//')      # minimal version: 0.1.0
-
 [ "$minor" -lt 1 ] && 1>&2 echo 'ERROR: md2pdf at least required in version 0.1.0, exiting' && exit 3
 
 find . -depth 1 -name '*.orig.md' -print0 | while read -r -d '' origFile ; do
@@ -33,4 +35,5 @@ find . -depth 1 -name '*.marp.md' -print0 | while read -r -d '' origFile ; do
         mv -v "${origFile/.md/.pdf}" "../${origFile/.marp.md/.pdf}"
     fi
 done
+
 # EOF
