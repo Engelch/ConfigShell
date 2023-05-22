73d36322f0c3ad286c71078fa7a3dcdf03516165
Fabio Scaccabarozzi <fabio.scaccabarozzi@vilea.ch>
2023-05-22 14:25:22 +0200

    doc/create-markdown+pdf.sh: make compatible with both MacOS find and GNU find, minor reformatting

--- doc/create-markdown+pdf.sh
+++ doc/create-markdown+pdf.sh
@@ -10,28 +10,28 @@
 # u230521 (no version# of its own)
 
 # check for minimal required version of md2pdf - not required if distributed as part of ConfigShell
-minor=$(md2pdf -V 2>&1 | sed -E 's/^[[:digit:]]+\.//' | sed -E 's/\..*//')      # minimal version: 0.1.0
-[ "$minor" -lt 1 ] && 1>&2 echo 'ERROR: md2pdf at least required in version 0.1.0, exiting' && exit 3
+minor=$(md2pdf -V 2>&1 | sed -E 's/^[[:digit:]]+\.//' | sed -E 's/\..*//') # minimal version: 0.1.0
+[ "$minor" -lt 1 ] && echo 1>&2 'ERROR: md2pdf at least required in version 0.1.0, exiting' && exit 3
 
-find . -depth 1 -name '*.orig.md' -print0 | while read -r -d '' origFile ; do
+find . -maxdepth 1 -name '*.orig.md' -print0 | while read -r -d '' origFile; do
     destMdFile=$(basename "$origFile" | sed 's,.orig,,')
     destPdfFile=${destMdFile/.md/.pdf}
     echo "$origFile → $destMdFile + $destPdfFile"
-    if [ "$1" != '-n' ] ; then
-        grep -Ev '\\small'  "$origFile"  | grep -Ev '\\normalsize' | grep -Ev '^colorlinks:' | \
-        grep -Ev '^---' >| "../$destMdFile"
+    if [ "$1" != '-n' ]; then
+        grep -Ev '\\small' "$origFile" | grep -Ev '\\normalsize' | grep -Ev '^colorlinks:' |
+            grep -Ev '^---' >|"../$destMdFile"
         /opt/ConfigShell/bin/md2pdf -f -o "../$destPdfFile" "$origFile"
     fi
 done
 
-find . -depth 1 -name '*.marp.md' -print0 | while read -r -d '' origFile ; do
+find . -maxdepth 1 -name '*.marp.md' -print0 | while read -r -d '' origFile; do
     destMdFile=$(basename "$origFile" | sed 's,.marp,,')
     destPdfFile=${destMdFile/.md/.pdf}
     echo "$origFile → $destMdFile + $destPdfFile"
-    if [ "$1" != '-n' ] ; then
-        grep -Ev '\\small' "$origFile" | grep -Ev '\\normalsize' | grep -Ev '^colorlinks:' | \
-        grep -Ev '^---' >| "../$destMdFile"
-        marp2pdf  "$origFile"
+    if [ "$1" != '-n' ]; then
+        grep -Ev '\\small' "$origFile" | grep -Ev '\\normalsize' | grep -Ev '^colorlinks:' |
+            grep -Ev '^---' >|"../$destMdFile"
+        marp2pdf "$origFile"
         mv -v "${origFile/.md/.pdf}" "../${origFile/.marp.md/.pdf}"
     fi
 done
