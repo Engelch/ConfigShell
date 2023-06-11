a5dcf241876dd0fc6348f92787bc2de42fceb52b
Christian ENGEL <engel-ch@outlook.com>
2023-06-11 11:54:38 +0200

    podman-test.sh:better self-documentation

--- bin/podman-test.sh
+++ bin/podman-test.sh
@@ -1,5 +1,10 @@
-echo "Currently (2023-06), podman runs seems to run the architecture" 
-echo "that was last run. Let's test it:"
+[ "$(uname)" != Darwin ] && 1>&2 echo "script to test different architectures makes only sense on Darwin alias OSX." && exit 1
+
+echo "date --202306"
+echo podman has the behaviour to keep the last architeture
+echo which was used with the previous podman command.
+echo "Let's test it..."
+echo ""
 echo 1. podman with explicit architecdture arm64 executes...
 podman run -q --rm --arch=arm64 ubuntu uname -m 
 echo 2. podman without an architecture set executes....
