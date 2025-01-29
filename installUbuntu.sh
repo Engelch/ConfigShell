ac8d3654188b183fd80fa49d3f16ae8dc506c458
Christian ENGEL <engel-ch@outlook.com>
2025-01-29 22:08:45 +0100

    installUbuntu.sh added to set up an Ubuntu development system

---
+++ installUbuntu.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y unzip ansible python3-pip bumpversion ruby gnutls-bin gpg vim python3-jinja2 zsh curl openssl openvpn tree thefuck
+
+sudo snap install j2
+sudo snap install bruno
+
+sudo snap install --classic go
+sudo snap install --classic opentofu
+sudo snap install --classic aws-cli
+sudo snap install --classic rustup
+
+rustup default stable
+rustup update
+
+sudo apt-get -y autoremove
