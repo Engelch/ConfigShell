d4eff3fe1844bb052368da5ab7cfbaa24e2016d6
Christian ENGEL <engel-ch@outlook.com>
2025-08-18 00:45:09 +0200

    tofu.pkg.sh v0.0.3 allowing install and force-install

--- pkg/tofu.pkg.sh
+++ pkg/tofu.pkg.sh
@@ -1,48 +1,88 @@
 #!/usr/bin/env bash
 
-function opentofuRepo() {
-  if [ ! -f  /etc/apt/keyrings/opentofu-repo.gpg ]  ; then
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
-    curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | \
-      sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
-    sudo chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
-  else
-    echo opentofu keyrings existing /etc/apt/keyrings/opentofu-repo.gpg
-  fi
 
-  if [ ! -f /etc/apt/sources.list.d/opentofu.list ] ; then
-    echo \
-      "deb [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/any/ any main
-deb-src [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/any/ any main" | \
-    sudo tee /etc/apt/sources.list.d/opentofu.list > /dev/null
-    sudo chmod a+r /etc/apt/sources.list.d/opentofu.list
-  else
-    echo opentofu repo already added as /etc/apt/sources.list.d/opentofu.list
-  fi
+function loadLib() {
+	########################################################################################
+	# ConfigShell lib 1.1 (codebase 1.0.0)
+	bashLib="/opt/ConfigShell/lib/bashlib.sh"
+	[ ! -f "$bashLib" ] && 1>&2 echo "bash-library $bashLib not found" && exit 127
+	# shellcheck source=/opt/ConfigShell/lib/bashlib.sh
+	source "$bashLib"
+	unset bashLib
+}
+
+function opentofuRepoInstall() {
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
+  curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | \
+    sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
+  
+  sudo rsync -av "${appDir}/tofu.pkg.d/files/" /
+  sudo chmod 644 /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
+  sudo chown root:root /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
+  sudo chmod 644 /etc/apt/sources.list.d/opentofu.list
+  sudo chown root:root /etc/apt/sources.list.d/opentofu.list
 
   sudo apt-get update
   sudo apt-get install -y tofu
+  return $?
+}
+
+function opentofuRepoStatus() {
+  which tofu &>/dev/null && echo -n tofu command found: \
+        && which tofu \
+        && [ -f /etc/apt/keyrings/opentofu.gpg ] && echo '  /etc/apt/keyrings/opentofu.gpg found' \
+        && [ -f /etc/apt/keyrings/opentofu-repo.gpg ] && echo '  /etc/apt/keyrings/opentofu-repo.gpg found' \
+        && [ -f /etc/apt/sources.list.d/opentofu.list ] && echo '  /etc/apt/sources.list.d/opentofu.list found' \
+	      && echo -n '  ' && dpkg -l tofu | grep ii 
+  which tofu &>/dev/null || errorExit 1 tofu command not found 
+}
+
+function recordSuccess() {
+  [ ! -d /etc/configshell.pkg/installed ] && sudo mkdir -p /etc/configshell.pkg/installed 
+  [ ! -d /etc/configshell.pkg/installed ] && errorExit 10 cannot create configshell.pkg/installed
+  _date="$(date --utc '+%y%m%d_%H:%M')"
+  echo $_date::$(dpkg -l $1 | grep ^ii)  | sudo tee /etc/configshell.pkg/installed/$1.$_date
+  find /etc/configshell.pkg/error/$1\* -exec /bin/rm -f {} \; &>/dev/null
+  find /etc/configshell.pkg/uninstalled/$1\* -exec /bin/rm -f {} \; &>/dev/null
+}
+
+function recordError() {
+  [ ! -d /etc/configshell.pkg/error ] && sudo mkdir -p /etc/configshell.pkg/error 
+  [ ! -d /etc/configshell.pkg/error ] && errorExit 11 cannot create configshell.pkg/error
+  _date="$(date --utc '+%y%m%d_%H:%M')"
+  echo $_date::$1  | sudo tee /etc/configshell.pkg/error/$1.$_date
+  find /etc/configshell.pkg/success/$1\* -exec /bin/rm -f {} \; &>/dev/null
+  find /etc/configshell.pkg/uninstalled/$1\* -exec /bin/rm -f {} \; &>/dev/null
 }
 
 
-
 function main() {
+  loadLib
+  appDir="$(dirname "$0")"
+  [ ! -d "$appDir/tofu.pkg.d/" ] && errorExit 3 tofu.pkg.d not found
   if [ -z "$1" ] ; then
     command="status"
   else
     command="$1"
   fi
   case "$command" in
-    install) opentofuRepo 
+    force-install) opentofuRepoInstall; res=$?
+	    [ "$res" -eq 0 ] && recordSuccess tofu
+	    [ "$res" -ne 0 ] && recordError tofu
       ;;
-    status) command -v tofu && echo tofu command found  \
-        && [ -f  /etc/apt/keyrings/opentofu-repo.gpg ] && echo '  /etc/apt/keyrings/opentofu-repo.gpg found' \
-        && [ ! -f /etc/apt/sources.list.d/opentofu.list ] && echo '/etc/apt/sources.list.d/opentofu.list found'
-      command -v tofu || { echo tofu command not found ; exit 1 ; }
+    install) 
+      which tofu &>/dev/null && echo tofu already found.
+      which tofu &>/dev/null || { opentofuRepoInstall ; res=$?
+	      [ "$res" -eq 0 ] && recordSuccess tofu
+	      [ "$res" -ne 0 ] && recordError tofu
+      }
       ;;
-    *) echo command not found
-      exit 1
+    status) opentofuRepoStatus 
+      ;;
+    version) echo 0.0.3
+      ;;
+    *) errorExit 9 'command mode not found, currently supported: install, force-install, status, version'
       ;;
   esac
 }
