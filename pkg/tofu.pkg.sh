a59df3cd25bae0c48848ce7144ceb828d89f3211
Christian ENGEL <engel-ch@outlook.com>
2025-08-17 22:51:40 +0200

    tofu.pkg.sh better documentation

--- pkg/tofu.pkg.sh
+++ pkg/tofu.pkg.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function opentofuRepo() {
-  if [ ! -f  /etc/apt/keyrings ]  ; then
+  if [ ! -f  /etc/apt/keyrings/opentofu-repo.gpg ]  ; then
     sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://get.opentofu.org/opentofu.gpg | sudo tee /etc/apt/keyrings/opentofu.gpg >/dev/null
     curl -fsSL https://packages.opentofu.org/opentofu/tofu/gpgkey | \
       sudo gpg --no-tty --batch --dearmor -o /etc/apt/keyrings/opentofu-repo.gpg >/dev/null
     sudo chmod a+r /etc/apt/keyrings/opentofu.gpg /etc/apt/keyrings/opentofu-repo.gpg
   else
-    echo opentofu keyrings existing
+    echo opentofu keyrings existing /etc/apt/keyrings/opentofu-repo.gpg
   fi
 
   if [ ! -f /etc/apt/sources.list.d/opentofu.list ] ; then
@@ -17,6 +17,8 @@ function opentofuRepo() {
 deb-src [signed-by=/etc/apt/keyrings/opentofu.gpg,/etc/apt/keyrings/opentofu-repo.gpg] https://packages.opentofu.org/opentofu/tofu/any/ any main" | \
     sudo tee /etc/apt/sources.list.d/opentofu.list > /dev/null
     sudo chmod a+r /etc/apt/sources.list.d/opentofu.list
+  else
+    echo opentofu repo already added as /etc/apt/sources.list.d/opentofu.list
   fi
 
   sudo apt-get update
